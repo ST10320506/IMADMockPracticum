c76b30e93c3664d83698cb20f4dbd6dbd6f52288
lab_services_student <st10320506@vcconnect.edu.za>
2025-06-18 09:57:22 +0200

    Intent created on the Second Screen to navigate back to the Main Activity Screen

--- app/src/main/java/vcmsa/ci/imadmockpracticum/SecondScreen.kt
+++ app/src/main/java/vcmsa/ci/imadmockpracticum/SecondScreen.kt
@@ -1,5 +1,6 @@
 package vcmsa.ci.imadmockpracticum
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
@@ -26,43 +27,61 @@ class SecondScreen : AppCompatActivity() {
         val btnReturn = findViewById<Button>(R.id.btnReturn)
 
         //Parallel Array passed through Intent (from MainActivity)
-        val itemNames = intent.getStringArrayListExtra("itemNames")?: arrayListOf()
-        val categories = intent.getStringArrayListExtra("categories")?: arrayListOf()
-        val quantities = intent.getIntegerArrayListExtra("quantities")?: arrayListOf()
-        val comments = intent.getStringArrayListExtra("comments")?: arrayListOf()
+        val itemNames = intent.getStringArrayListExtra("itemNames") ?: arrayListOf()
+        val categories = intent.getStringArrayListExtra("categories") ?: arrayListOf()
+        val quantities = intent.getIntegerArrayListExtra("quantities") ?: arrayListOf()
+        val comments = intent.getStringArrayListExtra("comments") ?: arrayListOf()
 
         //displays array on screen **
         fun displayList() {
-        val output = StringBuilder()
-        //Table Headings
-        output.append(String.format("\n", "Number ID", "Item Name",	"Category",	"Quantity",	"Comment"))
-        output.append("---------------------------------/n")
-        for (i in itemNames.indices) {
-            output.append(String.format( "\n",
-                i + 1,
-                itemNames[i],
-                categories[i],
-                quantities[i],
-                comments[i]
-            ))
-        }
-
-        //Shows list
-        tvListDisplay.text = output.toString()
-
-        fun btnFilterList() {
             val output = StringBuilder()
+            //Table Headings
+            output.append(
+                String.format(
+                    "\n",
+                    "Number ID",
+                    "Item Name",
+                    "Category",
+                    "Quantity",
+                    "Comment"
+                )
+            )
+            output.append("---------------------------------/n")
             for (i in itemNames.indices) {
-                if (quantities[i] >= 2) {
-                    output.append("${itemNames[i]} (Quantity: ${quantities[i]})\n")
+                output.append(
+                    String.format(
+                        "\n",
+                        i + 1,
+                        itemNames[i],
+                        categories[i],
+                        quantities[i],
+                        comments[i]
+                    )
+                )
+            }
+
+            //Shows list
+            tvListDisplay.text = output.toString()
+
+            fun btnFilterList() {
+                val output = StringBuilder()
+                for (i in itemNames.indices) {
+                    if (quantities[i] >= 2) {
+                        output.append("${itemNames[i]} (Quantity: ${quantities[i]})\n")
+                    }
                 }
             }
-        }
 
-        //Button to display list
-        btnDisplayList.setOnClickListener {
-            tvListDisplay.text = output.toString()
-        }
-    }// end of onCreate
-}// end of SecondScreen
-    //complete code
+            //Button to display list
+            btnDisplayList.setOnClickListener {
+                tvListDisplay.text = output.toString()
+            }
+
+            //Button to return to Main Activity Screen
+            btnReturn.setOnClickListener {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+        }// end of onCreate
+    }//end of SecondScreen
+}
